8e3a8e3f11e7e7c34d9f377027c39c8ddb01a41d
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-12 00:10:46 +0100

    ex10

---
+++ ex10.js
@@ -0,0 +1,5 @@
+//ex9 Passa’t dos valors, retorni cert o falç si el valor i el tipus de dades són el mateix.
+
+var mateixesDades = (var1,var2) => typeof var1 === typeof var2;
+
+console.log(mateixesDades("prova","dies"))

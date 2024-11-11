00f3ff2616be87a84379dda7773bc227ffc0db66
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-11 23:47:52 +0100

    ex5

--- ex5.js
+++ ex5.js
@@ -1,35 +1,27 @@
 //ex5 Passa’t un String, retorni el número de vocals que té
 
 function contarVocals(frase){
-    var a = 0
-
-    var e = 0
-
-    var iF = 0
-
-    var o = 0
-
-    var u = 0
+    var vocals = 0
 
     for (i = 0; i < frase.length; i++){
         if (frase.charAt(i) === "a"){
-            a++
+            vocals++
         }
         else if (frase.charAt(i) === "e"){
-            e++
+            vocals++
         }
-        else if (frase.charAt(iF) === "i"){
-            iF++
+        else if (frase.charAt(i) === "i"){
+            vocals++
         }
         else if (frase.charAt(i) === "o"){
-            o++
+            vocals++
         }
         else if (frase.charAt(i) === "u"){
-            u++
+            vocals++
         }
     }
 
-    console.log("Vocals trobades: "+"a"+a+" e"+e+" i"+iF+" o"+o+" u"+u)
+    console.log("Vocals trobades: "+vocals)
 }
 
 contarVocals("hola")

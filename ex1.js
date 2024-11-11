447079e4b64510e9d1d1c458bbe7a4de46009300
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-11 22:32:02 +0100

    Initial commit

---
+++ ex1.js
@@ -0,0 +1,7 @@
+//Passa el teu nom i cognoms com a paràmetres i els mostri per consola
+
+function nomcognom(nom,cognom1,cognom2){
+    console.log(nom+" "+cognom1+" "+cognom2);
+}
+
+nomcognom("Òscar","Mollinedo","Echevarría");

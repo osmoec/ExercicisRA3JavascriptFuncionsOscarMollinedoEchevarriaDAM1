235b9e0caaed7a4a828b680df891c22754d36216
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-11 23:52:53 +0100

    ex6

--- ex6.js
+++ ex6.js
@@ -1,3 +1,5 @@
+//ex6 Passa 3 enters com a paràmetres (el tercer ha de tenir un valor per defecte) i retorni la suma dels tres.
+
 function sumardosnumerosmestres(numero1,numero2) {
     console.log(numero1 + numero2+3);
 }

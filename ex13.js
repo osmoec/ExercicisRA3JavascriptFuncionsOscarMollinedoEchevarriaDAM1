1f9de62b9304d4c6797d9001d82c9fbcf0c96cad
Òscar Mollinedo Echevarría <oscar.mollinedo@institutvidreres.cat>
2024-11-13 14:11:08 +0100

    ex13

--- ex13.js
+++ ex13.js
@@ -1,7 +1,15 @@
 //ex13 Passa’t dos valors, retorni el que no és null.
 
-var nonull = (variable) => variable != null
+var nonull = (variable1,variable2) => {
+    if (variable1 !== null && variable2 === null) {
+        return variable1
+    }
+    else if (variable2 !== null && variable1 === null) {
+        return variable2
+    }
+    else{
+        return "els dos son null"
+    }
+}
 
-var prova = 1
-
-console.log(nonull(prova))
+console.log(nonull(null, 1))
